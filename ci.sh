5a8b109cdca450460fc56a68aaf3d5363b2581f3
ivmarkov <ivan.markov@gmail.com>
2025-10-09 18:17:48 +0000

    Exclude nrf54 from the CI for now

--- ci.sh
+++ ci.sh
@@ -6,9 +6,10 @@ export RUSTFLAGS=-Dwarnings
 
 cargo clippy -p nrf-802154-sys --features nrf52
 cargo clippy -p nrf-802154-sys --features nrf53 --target thumbv8m.main-none-eabi
-cargo clippy -p nrf-802154-sys --features nrf54l-ns --target thumbv8m.main-none-eabihf
-cargo clippy -p nrf-802154-sys --features nrf54l-s --target thumbv8m.main-none-eabihf
-cargo clippy -p nrf-802154-sys --features nrf54h --target thumbv8m.main-none-eabihf
+# NRF_CONFIG_CPU_FREQ_MHZ issues
+#cargo clippy -p nrf-802154-sys --features nrf54l-ns --target thumbv8m.main-none-eabihf
+#cargo clippy -p nrf-802154-sys --features nrf54l-s --target thumbv8m.main-none-eabihf
+#cargo clippy -p nrf-802154-sys --features nrf54h --target thumbv8m.main-none-eabihf
 
 cargo clippy -p nrf-802154 --features nrf52805
 cargo clippy -p nrf-802154 --features nrf52810
